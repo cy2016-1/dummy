af86fcc84af397bb6241b9a011e365822e0649c0
liyq <liyq@Clion1.clion.local>
2023-12-10 18:03:33 +0800

    add 35 file daplink.cfg

---
+++ firmware/dummy-35motor-fw/cmake-build-debug/CMakeFiles/Ctrl-Step-STM32-fw.elf.dir/Ctrl/Signal/led_base.cpp.obj.d
@@ -0,0 +1,19 @@
+CMakeFiles/Ctrl-Step-STM32-fw.elf.dir/Ctrl/Signal/led_base.cpp.obj: \
+ /home/liyq/fAN_Dummy/dummy/firmware/dummy-35motor-fw/Ctrl/Signal/led_base.cpp \
+ /home/liyq/fAN_Dummy/dummy/firmware/dummy-35motor-fw/Ctrl/Signal/led_base.h \
+ /usr/local/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/arm-none-eabi/include/c++/12.3.1/cstdint \
+ /usr/local/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/arm-none-eabi/include/c++/12.3.1/arm-none-eabi/thumb/v7-m/nofp/bits/c++config.h \
+ /usr/local/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/arm-none-eabi/include/c++/12.3.1/arm-none-eabi/thumb/v7-m/nofp/bits/os_defines.h \
+ /usr/local/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/arm-none-eabi/include/c++/12.3.1/arm-none-eabi/thumb/v7-m/nofp/bits/cpu_defines.h \
+ /usr/local/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/arm-none-eabi/include/c++/12.3.1/pstl/pstl_config.h \
+ /usr/local/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/lib/gcc/arm-none-eabi/12.3.1/include/stdint.h \
+ /usr/local/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/arm-none-eabi/include/stdint.h \
+ /usr/local/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/arm-none-eabi/include/machine/_default_types.h \
+ /usr/local/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/arm-none-eabi/include/sys/features.h \
+ /usr/local/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/arm-none-eabi/include/_newlib_version.h \
+ /usr/local/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/arm-none-eabi/include/sys/_intsup.h \
+ /usr/local/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/arm-none-eabi/include/sys/_stdint.h \
+ /home/liyq/fAN_Dummy/dummy/firmware/dummy-35motor-fw/Ctrl/Motor/motor.h \
+ /home/liyq/fAN_Dummy/dummy/firmware/dummy-35motor-fw/Ctrl/Motor/motion_planner.h \
+ /home/liyq/fAN_Dummy/dummy/firmware/dummy-35motor-fw/Ctrl/Sensor/Encoder/encoder_base.h \
+ /home/liyq/fAN_Dummy/dummy/firmware/dummy-35motor-fw/Ctrl/Driver/driver_base.h
